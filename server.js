3ff494a3b2d27a8789c4a422bb3a4669f06888f1
Joatan <joatansampaio@live.com>
2023-11-26 13:52:22 -0600

    Enabling CORS

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const jsonServer = require('json-server');
+const cors = require('cors');
 
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.use(cors({
+    origin: 'https://todo-app-vuejs-joatansampaio.vercel.app/', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, 
+}));
+
+
 server.use(
     jsonServer.rewriter({
-        "/*": "/$1",
+        '/*': '/$1',
     })
 );
 server.use(router);
-server.listen(3000, () => {
-    console.log("JSON Server is running");
-});
 
-module.exports = server;
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
