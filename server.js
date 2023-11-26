ccf92f68e1da68a41fe3969e79f96069e08575e7
Joatan <joatansampaio@live.com>
2023-11-26 13:36:20 -0600

    Changed from Module to CommonJS

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
-import jsonServer from 'jsonServer'
-
+const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-
 server.use(
-
-    jsonServer.rewrite({
+    jsonServer.rewriter({
         "/*": "/$1",
     })
 );
-
 server.use(router);
-
 server.listen(3000, () => {
-    console.log("Server running at http://localhost:3000")
-})
+    console.log("JSON Server is running");
+});
 
-export default server;
+module.exports = server;
